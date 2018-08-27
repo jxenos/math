48140142c69479bda8f26270d9fb1694c7806986
Jorj Xenos <jxenos@earthcam.com>
2018-08-27 10:42:10 -0400

    incorperate the csv parser into the interpreter and try to split into functions

--- lin_interp.c
+++ lin_interp.c
@@ -3,43 +3,97 @@
 
 //https://www.gnu.org/software/libc/manual/html_node/Argument-Syntax.html#Argument-Syntax
 
-typedef struct cords {
+typedef struct coords
+{
 	double x;
 	double y;
-} Cords;
-int main(int argc, char** argv){
-	int length = 10;
-	Cords cords[length];
-	int i;
-	for (i = 0; i < length; i++){
-	}
-	float x[] = {1,100,200,300,450,600,800,1000,1300,1600};
-	float y[] = {550,509.5,468,437,398,361.5,331,297,265.5,236};
+} Coords;
 
-	float input = 0;
-	float output;
-	int i;
-	float length = sizeof(x)/sizeof(*x);
+double linear_inter(Coords *, double input)
+{
+	float length = sizeof(x) / sizeof(*x);
+	double output;
 
-	// printf("%f\n%f\n%f\n%f\n", length, x[0], x[(int) length-1], input);
-
-	if(input < x[0] || input > x[(int) length-1]){
+	if (input < x[0] || input > x[(int)length - 1])
+	{
 		printf("Error: Outside bounds\n");
 		return 1;
 	}
 
-	for(i=0; i<length; i++){
-		if(input == x[i]){
+	int i;
+	for (i = 0; i < length; i++)
+	{
+		if (input == x[i])
+		{
 			output = y[i];
 			break;
 		}
-		if(input < x[i]){
-			output = (y[i]-y[i-1])*(input-x[i-1])/(x[i]-x[i-1])+y[i-1];
+		if (input < x[i])
+		{
+			output = (y[i] - y[i - 1]) * (input - x[i - 1]) / (x[i] - x[i - 1]) + y[i - 1];
 			break;
 		}
 	}
 
-	printf("%f\n", output);
+	return output;
+};
+
+#include <stdlib.h>
+#include <string.h>
+#define MAXFLDS 200   /* maximum possible number of fields */
+#define MAXFLDSIZE 32 /* longest possible field + 1 = 31 byte field */
+void parse(char *record, char *delim, char arr[][MAXFLDSIZE], int *fldcnt)
+{
+	char *p = strtok(record, delim);
+	int fld = 0;
+
+	while (p)
+	{
+		strcpy(arr[fld], p);
+		fld++;
+		p = strtok('\0', delim);
+	}
+	*fldcnt = fld;
+}
+
+Coords *csv_parser(char *csv_file)
+{
+	char tmp[1024] = {0x0};
+	int fldcnt = 0;
+	char arr[MAXFLDS][MAXFLDSIZE] = {0x0};
+	int recordcnt = 0;
+	FILE *in = fopen(csv_file, "r"); /* open file on command line */
+
+	if (in == NULL)
+	{
+		perror("File open error");
+		exit(EXIT_FAILURE);
+	}
+	while (fgets(tmp, sizeof(tmp), in) != 0) /* read a record */
+	{
+		int i = 0;
+		recordcnt++;
+		printf("Record number: %d\n", recordcnt);
+		parse(tmp, ",", arr, &fldcnt); /* whack record into fields */
+		for (i = 0; i < fldcnt; i++)
+		{ /* print each field */
+			printf("\tField number: %3d==%s\n", i, arr[i]);
+		}
+	}
+	fclose(in);
+	return 0;
+}
+
+int main(int argc, char **argv)
+{
+	//float x[] = {1, 100, 200, 300, 450, 600, 800, 1000, 1300, 1600};
+	//float y[] = {550, 509.5, 468, 437, 398, 361.5, 331, 297, 265.5, 236};
+
+	Coords *coords = csv_parser("camera_data.cfg");
+
+	double input = 0;
+
+	linear_inter(coords, input);
 
 	return 0;
 }
