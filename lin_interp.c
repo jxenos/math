eb7bfa3ae85adc5bc072a42a3071d825b78b17a2
Jorj Xenos <jxenos@earthcam.com>
2018-08-29 11:49:26 -0400

    first attempt at sorting

--- lin_interp.c
+++ lin_interp.c
@@ -21,17 +21,28 @@ typedef struct coords_array
 	int length;
 } Coords_Array;
 
+int compare(const void *a, const void *b)
+{
+	return ((Coords *)a)->x - ((Coords *)b)->x;
+}
+
 double linear_inter(Coords_Array coords_array, double input)
 {
-
 	Coords *coords = coords_array.coords_array;
 	int length = coords_array.length;
 
-	printf("input: %f\n", input);
+	qsort(*coords, length, sizeof(*coords), compare);
+
+	//printf("input: %f\n", input);
 
 	double output;
-	printf("length: %f\n", length);
-	printf("0.x: %f, last.x: %f\n", coords[0].x, coords[length - 1].x);
+	//printf("length: %d\n", length);
+	//printf("0.x: %f, last.x: %f\n", coords[0].x, coords[length - 1].x);
+	int i;
+	for (i = 0; i < length; i++;)
+	{
+		printf("%d: x: %f y: %f\n", i, coords[i].x, coords[i].y);
+	}
 
 	if (input < coords[0].x || input > coords[length - 1].x)
 	{
@@ -56,20 +67,6 @@ double linear_inter(Coords_Array coords_array, double input)
 
 	return output;
 };
-
-void parse(char *record, char *delim, char arr[][MAXFLDSIZE], int *fldcnt)
-{
-	char *p = strtok(record, delim);
-	int fld = 0;
-
-	while (p)
-	{
-		strcpy(arr[fld], p);
-		fld++;
-		p = strtok('\0', delim);
-	}
-	*fldcnt = fld;
-}
 
 Coords_Array csv_parser(char *csv_file)
 {
@@ -102,7 +99,6 @@ Coords_Array csv_parser(char *csv_file)
 	int i = file_length;
 	while (i--)
 	{
-		//	if (file_data[i] == delimiter)
 		if (file_data[i] == delimiter[0])
 			rowcnt++;
 	}
@@ -126,58 +122,6 @@ Coords_Array csv_parser(char *csv_file)
 		temp = strtok(NULL, delimiter);
 		data[i].y = atof(temp);
 	}
-
-	/*
-	for (i = 0; i < rowcnt; i++)
-	{
-		temp = strtok(*file_data, "\n");
-		data[i].x = atof(strtok(*temp, delimiter));
-		data[i].y = atof(*temp);
-	}
-*/
-	//======================================================================
-	/*
-		char tmp[1024] = {0x0};
-	int fldcnt = 0;
-	char arr[MAXFLDS][MAXFLDSIZE] = {0x0};
-
-	while (fgets(tmp, sizeof(tmp), in) != 0) // read a record
-	{
-		int i = 0;
-		rowcnt++;
-		printf("Record number: %d\n", rowcnt);
-		parse(tmp, ",", arr, &fldcnt); // whack record into fields
-		for (i = 0; i < fldcnt; i++)
-		{ // print each field
-			printf("\tField number: %3d==%s\n", i, arr[i]);
-		}
-	}
-	fclose(in);
-*/
-	/*
-	Coords *coords = malloc(sizeof(Coords) * 11);
-
-	coords[0].x = 1;
-	coords[1].x = 100;
-	coords[2].x = 200;
-	coords[3].x = 300;
-	coords[4].x = 450;
-	coords[5].x = 600;
-	coords[6].x = 800;
-	coords[7].x = 1000;
-	coords[8].x = 1300;
-	coords[9].x = 1600;
-
-	coords[0].y = 550;
-	coords[1].y = 509.5;
-	coords[2].y = 468;
-	coords[3].y = 437;
-	coords[4].y = 398;
-	coords[5].y = 361;
-	coords[6].y = 331;
-	coords[7].y = 297;
-	coords[8].y = 265.5;
-	coords[9].y = 236;*/
 
 	return (Coords_Array){.coords_array = data, .length = rowcnt};
 }
@@ -222,7 +166,7 @@ int main(int argc, char **argv)
 		}
 	}
 
-	printf("c: %s\ni: %f\n", camera_config, input);
+	//printf("c: %s\ni: %f\n", camera_config, input);
 
 	Coords_Array coords_array = csv_parser(camera_config);
 
